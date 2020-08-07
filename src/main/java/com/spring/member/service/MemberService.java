81bbc1a6ed55ca0c51e941905e5d3ee5b69c83cb
sh6137 <33920015+sh6137@users.noreply.github.com>
2020-08-07 13:26:21 +0900

    login 다시

--- src/main/java/com/spring/member/service/MemberService.java
+++ src/main/java/com/spring/member/service/MemberService.java
@@ -2,17 +2,10 @@ package com.spring.member.service;
 
 import java.util.HashMap;
 
-import javax.servlet.http.HttpSession;
-
 import com.spring.member.vo.MemberVo;
 
 public interface MemberService {
 
-	void setJoin(HashMap<String, Object> map);
-	//회원 로그인 체크
-	boolean loginCheck(MemberVo vo, HttpSession session);
-	//회원 로그인 정보
-	MemberVo viewMember(MemberVo vo);
-	//회원 로그아웃
-	void logout(HttpSession session);
+	public MemberVo login(HashMap<String, Object> map);
+
 }

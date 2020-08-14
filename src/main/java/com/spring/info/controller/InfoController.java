11e1d198af413882adb06ac662d1f82a42537e43
sh6137 <33920015+sh6137@users.noreply.github.com>
2020-08-14 19:01:59 +0900

    끄

--- src/main/java/com/spring/info/controller/InfoController.java
+++ src/main/java/com/spring/info/controller/InfoController.java
@@ -3,6 +3,8 @@ package com.spring.info.controller;
 import java.util.HashMap;
 import java.util.List;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -95,10 +97,11 @@ public class InfoController {
 	}
 
 	@RequestMapping("/INFO/DropInfo")
-	public ModelAndView deleteInfo(@RequestParam HashMap<String, Object> map) {
+	public ModelAndView deleteInfo(HttpSession session, @RequestParam HashMap<String, Object> map) {
 		System.out.println("/INFO/DropInfo의 map : " + map);
 		infoService.deleteInfo(map);
-
+		session.removeAttribute("login");
+		session.invalidate();
 		ModelAndView mv = new ModelAndView();
 		mv.setViewName("redirect:/");
 
